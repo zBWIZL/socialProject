413e09083c2feace466e46df2282bd3fac1dcd5a
bradtucker <bradtucker12345@gmail.com>
2024-07-18 15:55:47 -0400

    first

---
+++ socialMedia/core/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('settings', views.settings, name='settings'),
+    path('upload', views.upload, name='upload'),
+    path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
+    path('profile/<str:pk>', views.profile, name='profile'),
+    path('like-post', views.like_post, name='like-post'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
+    path('generate/', views.GenerateImageView.as_view(), name='generate-image'),
+    path('generate_form/', views.generate_image_form, name='generate-image-form'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
