d49988467799f54badaf22f37710b12137668d2d
bradtucker <bradtucker12345@gmail.com>
2024-07-18 17:13:06 -0400

    html

--- socialMedia/social_book/settings.py
+++ socialMedia/social_book/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-from decouple import config, Csv
+# from decouple import config, Csv
 
 load_dotenv()
 
